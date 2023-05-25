05356775df30e32a14de9daafa749a6ec94954a4
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-05-25 19:52:32 +0300

    refactoring & improve calculate MA function

--- utils.py
+++ utils.py
@@ -1,8 +1,19 @@
+import decimal
+import json
+
 import requests
 
 
-def calculate_ma(prices):
-    return sum(prices) / len(prices)
+
+def calculate_ma(symbol, interval, limit, client):
+    klines = client.klines(symbol=symbol, interval=interval)
+    closing_prices = [float(kline[4]) for kline in klines]
+    if len(closing_prices) < limit:
+        return None
+
+    ma = sum(closing_prices[-limit:]) / limit
+
+    return ma
 
 
 # Function to send a message via Telegram
@@ -15,4 +26,21 @@ def send_telegram_message(message, telegram_api_url, telegram_bot_token, telegra
     response = requests.post(url, json=data)
     if response.status_code != 200:
         print(f"Failed to send Telegram message. Error: {response.text}")
+
+
+def get_candles(symbol, interval, limit, close_prices, api_url):
+    endpoint = f"{api_url}/klines"
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+    response = requests.get(endpoint, params=params)
+    if response.status_code == 200:
+        candles = json.loads(response.text)
+        data = [decimal.Decimal(candle[4]) for candle in candles]  # Get close prices from candles
+        close_prices.extend(data)
+    else:
+        print("Failed to retrieve candles from Binance API.")
+        return []
 
